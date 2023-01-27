742d10e8cfe06fc106f4b7a5c4a1bf3176eb7b0e
PLUGGIN <Soulgr33n@gmail.com>
2023-01-27 17:37:23 +0200

    Initial commit

---
+++ src/main/java/com/ontu/LAB5/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.ontu.LAB5;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public
+class HelloWorldController {
+    @GetMapping("/hello-world")
+    public String HelloWorld() {
+        return "Hello World";
+    }
+}
